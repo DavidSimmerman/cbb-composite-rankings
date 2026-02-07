95fae0020f9db36d03f229e62bd71f62acaf7408
David Simmerman <davidsimmermancs@gmail.com>
2026-02-07 00:39:43 -0500

    update team page graph options

--- app/[teamKey]/components/TeamCharts.tsx
+++ app/[teamKey]/components/TeamCharts.tsx
@@ -1,60 +1,134 @@
 'use client';
 
+import { allSources } from '@/app/components/columns';
+import SourcesFilter from '@/app/components/SourcesFilter';
+import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { Label } from '@/components/ui/label';
+import { Switch } from '@/components/ui/switch';
+import { Toggle } from '@/components/ui/toggle';
 import { ProfileRatingsHistory } from '@/lib/rankings/rankings';
+import { useMemo, useState } from 'react';
+import { PiChartLineBold } from 'react-icons/pi';
+import { RiCollapseVerticalLine, RiExpandVerticalLine, RiZoomInFill, RiZoomInLine } from 'react-icons/ri';
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
 
 export default function TeamCharts({ history }: { history: ProfileRatingsHistory }) {
-	console.log(history);
+	const [zoom, setZoom] = useState<boolean>(false);
+	const [rank, setRank] = useState<boolean>(false);
+	const [sourcesFilter, setSourcesFilter] = useState<string[]>([...allSources]);
 
 	const ratingData = Object.values(history).map(h => ({
 		date: h.date,
-		composite: h.avg_zscore,
-		kenpom: h.kp_rating_zscore,
-		evanmiya: h.em_rating_zscore,
-		barttorvik: h.bt_rating_zscore,
-		net: h.net_rank_zscore,
+		composite: rank ? -h.avg_zscore_rank : h.avg_zscore,
+		kenpom: rank ? -h.kp_rating_rank : h.kp_rating_zscore,
+		evanmiya: rank ? -h.em_rating_rank : h.em_rating_zscore,
+		barttorvik: rank ? -h.bt_rating_rank : h.bt_rating_zscore,
+		net: rank ? -h.net_rank : h.net_rank_zscore,
 		full: h
 	}));
 
 	const offensiveData = Object.values(history).map(h => ({
 		date: h.date,
-		composite: h.avg_offensive_zscore,
-		kenpom: h.kp_offensive_zscore,
-		evanmiya: h.em_offensive_zscore,
-		barttorvik: h.bt_offensive_zscore,
+		composite: rank ? -h.avg_offensive_zscore_rank : h.avg_offensive_zscore,
+		kenpom: rank ? -h.kp_offensive_rating_rank : h.kp_offensive_zscore,
+		evanmiya: rank ? -h.em_offensive_rating_rank : h.em_offensive_zscore,
+		barttorvik: rank ? -h.bt_offensive_rating_rank : h.bt_offensive_zscore,
 		full: h
 	}));
 
 	const defensiveData = Object.values(history).map(h => ({
 		date: h.date,
-		composite: h.avg_defensive_zscore,
-		kenpom: h.kp_defensive_zscore,
-		evanmiya: h.em_defensive_zscore,
-		barttorvik: h.bt_defensive_zscore,
+		composite: rank ? -h.avg_defensive_zscore_rank : h.avg_defensive_zscore,
+		kenpom: rank ? -h.kp_defensive_rating_rank : h.kp_defensive_zscore,
+		evanmiya: rank ? -h.em_defensive_rating_rank : h.em_defensive_zscore,
+		barttorvik: rank ? -h.bt_defensive_rating_rank : h.bt_defensive_zscore,
 		full: h
 	}));
 
+	const range = useMemo(() => {
+		if (zoom) {
+			return ['dataMin - 0.2', 'dataMax + 0.2'];
+		} else if (rank) {
+			return [-50, 0];
+		} else {
+			return [0, 3];
+		}
+	}, [zoom, rank]);
+
 	return (
-		<div className="flex flex-col md:flex-row w-full gap-4">
-			<div className="w-full flex flex-col gap-2">
-				<div className="text-muted-foreground ml-3">Rating</div>
-				<ChartCard chartData={ratingData} metric="rating" />
+		<div className="flex flex-col w-full gap-4 border border-neutral-800 rounded-lg p-4">
+			<div className="ml-auto mr-0 flex gap-2 z-1">
+				<Toggle className={`cursor-pointer [&_svg]:transition-transform [&_svg]:duration-200 ${zoom ? 'hover:[&_svg]:scale-75' : 'hover:[&_svg]:scale-125'}`} pressed={zoom} onPressedChange={setZoom}>
+					{zoom ? <RiCollapseVerticalLine /> : <RiExpandVerticalLine />}
+				</Toggle>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button className="cursor-pointer" variant="outline">
+							<PiChartLineBold />
+							{rank ? 'Rank' : 'Z-Score'}
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent className="">
+						<DropdownMenuGroup>
+							<DropdownMenuCheckboxItem
+								checked={!rank}
+								onCheckedChange={() => setRank(false)}
+								className="px-6 flex gap-0 justify-center cursor-pointer capitalize"
+							>
+								Z-Score
+							</DropdownMenuCheckboxItem>
+							<DropdownMenuCheckboxItem
+								checked={rank}
+								onCheckedChange={() => setRank(true)}
+								className="px-6 flex gap-0 justify-center cursor-pointer capitalize"
+							>
+								Rank
+							</DropdownMenuCheckboxItem>
+						</DropdownMenuGroup>
+					</DropdownMenuContent>
+				</DropdownMenu>
+				<SourcesFilter sourcesFilter={sourcesFilter} onChange={setSourcesFilter} />
 			</div>
-			<div className="w-full flex flex-col gap-2">
-				<div className="text-muted-foreground ml-3">Offensive Rating</div>
-				<ChartCard chartData={offensiveData} metric="offensive_rating" />
-			</div>
-			<div className="w-full flex flex-col gap-2">
-				<div className="text-muted-foreground ml-3">Defensive Rating</div>
-				<ChartCard chartData={defensiveData} metric="defensive_rating" />
+
+			<div className="flex flex-col md:flex-row gap-8 -mt-8">
+				<div className="w-full flex flex-col gap-1">
+					<div className="text-muted-foreground ml-3">Rating</div>
+
+					<ChartCard chartData={ratingData} metric="rating" range={range} sources={sourcesFilter} />
+				</div>
+				<div className="w-full flex flex-col gap-1">
+					<div className="text-muted-foreground ml-3">Offensive Rating</div>
+					<ChartCard chartData={offensiveData} metric="offensive_rating" range={range} sources={sourcesFilter} />
+				</div>
+				<div className="w-full flex flex-col gap-1">
+					<div className="text-muted-foreground ml-3">Defensive Rating</div>
+					<ChartCard chartData={defensiveData} metric="defensive_rating" range={range} sources={sourcesFilter} />
+				</div>
 			</div>
 		</div>
 	);
 }
 
-function ChartCard({ chartData, metric }: { chartData: Record<string, any>[]; metric: string }) {
+function ChartCard({
+	chartData,
+	metric,
+	range,
+	sources
+}: {
+	chartData: Record<string, any>[];
+	metric: string;
+	range: any[2];
+	sources: string[];
+}) {
 	const chartConfig = {
 		composite: {
 			label: 'Composite',
@@ -90,7 +164,7 @@ function ChartCard({ chartData, metric }: { chartData: Record<string, any>[]; me
 						tickMargin={8}
 						tickFormatter={value => value.replace(/^\d{4}-/, '')}
 					/>
-					<YAxis domain={['dataMin - 0.2', 'dataMax + 0.2']} hide />
+					<YAxis domain={range} hide />
 					<ChartTooltip
 						cursor={false}
 						content={
@@ -134,11 +208,22 @@ function ChartCard({ chartData, metric }: { chartData: Record<string, any>[]; me
 							/>
 						}
 					/>
-					<Line dataKey="composite" type="monotone" stroke="var(--color-purple-500)" strokeWidth={2} dot={false} />
-					<Line dataKey="kenpom" type="monotone" stroke="var(--color-blue-500)" strokeWidth={2} dot={false} />
-					<Line dataKey="evanmiya" type="monotone" stroke="var(--color-green-500)" strokeWidth={2} dot={false} />
-					<Line dataKey="barttorvik" type="monotone" stroke="var(--color-yellow-500)" strokeWidth={2} dot={false} />
-					<Line dataKey="net" type="monotone" stroke="var(--color-red-500)" strokeWidth={2} dot={false} />
+
+					{(sources.includes('Composite') || sources.length === 0) && (
+						<Line dataKey="composite" type="monotone" stroke="var(--color-purple-500)" strokeWidth={2} dot={false} />
+					)}
+					{(sources.includes('KenPom') || sources.length === 0) && (
+						<Line dataKey="kenpom" type="monotone" stroke="var(--color-blue-500)" strokeWidth={2} dot={false} />
+					)}
+					{(sources.includes('EvanMiya') || sources.length === 0) && (
+						<Line dataKey="evanmiya" type="monotone" stroke="var(--color-green-500)" strokeWidth={2} dot={false} />
+					)}
+					{(sources.includes('BartTorvik') || sources.length === 0) && (
+						<Line dataKey="barttorvik" type="monotone" stroke="var(--color-yellow-500)" strokeWidth={2} dot={false} />
+					)}
+					{(sources.includes('NET') || sources.length === 0) && (
+						<Line dataKey="net" type="monotone" stroke="var(--color-red-500)" strokeWidth={2} dot={false} />
+					)}
 				</LineChart>
 			</ChartContainer>
 		</Card>
--- app/[teamKey]/page.tsx
+++ app/[teamKey]/page.tsx
@@ -1,6 +1,9 @@
 import TitleBar from '@/components/TitleBar';
 import { getTeamProfile } from '@/lib/rankings/rankings';
 import TeamCharts from './components/TeamCharts';
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
 
 export default async function TeamPage({ params }: { params: Promise<{ teamKey: string }> }) {
 	const { teamKey } = await params;
@@ -9,7 +12,12 @@ export default async function TeamPage({ params }: { params: Promise<{ teamKey: 
 
 	return (
 		<div>
-			<TitleBar title={profile.team_name} />
+			<div className="relative flex items-center justify-center">
+				<Link href="/" className="absolute left-4 mt-10 cursor-pointer group">
+					< className="transition-transform duration-200 group-hover:-translate-x-1 size-6 text-neutral-400 hover:text-white" />
+				</Link>
+				<TitleBar title={profile.team_name} />
+			</div>
 			<div className="w-full mt-12">
 				<TeamCharts history={profile.ratings_history} />
 			</div>
