7b60e1de3b4598e0d010728273001e0dac0cfcdd
David Simmerman <davidsimmermancs@gmail.com>
2026-02-10 01:34:08 -0500

    fix cron job timings

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,7 +3,7 @@ export async function register() {
 		const cron = await import('node-cron');
 		const { updateRankings } = await import('./lib/rankings/rankings');
 
-		cron.schedule('0 6,12,14,17,19,20,21,22,23,12,2 * * *', () => {
+		cron.schedule('0 0,2,6,12,14,17,19,20,21,22,23 * * *', () => {
 			updateRankings(['kenpom', 'evanmiya', 'barttorvik', 'net']);
 		});
 
